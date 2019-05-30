88613da16527af1182d69ae209d8cdb05961c64b
“Patrick <pctennislessons@gmail.com>
2019-05-30 14:14:00 -0600

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |key, value|
+      value.each do |key2, value2|
+        new_hash[key2] = value2
+        new_hash[key2][:style] = []
+     end
+  end
+  languages.each do |key, value|
+      value.each do |key2, value2|
+        new_hash[key2][:style] << key
+      end
+    end
+  new_hash
 end
